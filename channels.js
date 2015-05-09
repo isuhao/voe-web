bf9241bbd871ef30fb8aadcf7ae99d436091b26c
jack <jack.wgm@gmail.com>
2015-05-09 12:14:30 +0800

    呵呵， html 也可以模块话， 使用 #include 哦！

--- channels.js
+++ channels.js
@@ -99,3 +99,92 @@ function show_edit_channel(rowid, channel_id) {
         mod_channel(rowid, channel_id);
     });
 }
+
+
+var tr_template = '<tr class="success">\
+                <td></td>\
+                <td>{channel_id}</td>\
+                <td>{channel_name}</td>\
+                <td>{channel_source_url}</td>\
+                <td><a href="detail.html?server_id={channel_source_server_id}" style="width: 100%">{channel_source_server_id}</a></td>\
+                <td>{channel_servers}</td>\
+        <td>\
+          <!-- Split button -->\
+        <div class="btn-group">\
+            <a type="button" class="btn btn-info" href="channel_flow.html?channel_id={channel_id}" target="_blank">详情</a>\
+            <button type="button" id="edit_channel_buttion_{rowid}" class="btn btn-info" data-toggle="modal" data-target="#edit_channel" \
+                onclick="show_edit_channel({rowid}, {channel_id})">编辑</button>\
+            <button type="button" class="btn btn-info" onclick="delete_channel({rowid}, {channel_id}, {channel_servers})">删除</button>\
+        </div>\
+        </td>\
+        </tr>';
+
+
+window.real_ready = function ()	// 在窗口加载的时候，调用登陆，并且请求服务器状态数据。
+{
+    $('#prepage').hide();
+    $('#nextpage').hide();
+
+    if (window.curpage > 1) {
+        $('#prepage').prop('href', 'channels.html?curpage={0}'.format((window.curpage | 0) - 1));
+        $('#prepage').show();
+    }
+
+    var page_size = 20;
+
+    $.ajax({
+        type: "POST",
+        url: "api/get_channels",
+        data: "pageSize=20&curPage=" + window.curpage,
+        success: function (data)	// 获取服务器状态数据.
+        {
+            var jsonobj = eval(data);
+
+            console.log("success status: " + jsonobj["success"]);
+            console.log("length: " + jsonobj["data"].length);
+
+            if (window.curpage < jsonobj["pageCount"]) {
+                $('#nextpage').show();
+                $('#nextpage').prop('href', 'channels.html?curpage={0}'.format((window.curpage | 0) + 1));
+            }
+
+            $('#curPage_indicator').html('当前第{curPage}页, 共{pageCount}页'.format(jsonobj));
+
+            window.current_data = jsonobj["data"];
+
+            for (var i = 0; i < jsonobj["data"].length; i++) {
+                var rowdata = jsonobj["data"][i];
+                rowdata.rowid = i;
+
+                if (rowdata.channel_name == null) {
+                    rowdata.channel_name = "未设定";
+                }
+
+                var newtr = tr_template.format(rowdata);
+
+                $("#the_table_body").append(newtr);
+
+
+            }
+        }
+    });
+};
+
+function format_ip(ips) {
+    var r = '';
+    for (var i = 0; i < ips.length; i++) {
+        if (r == '') {
+
+        }
+        else {
+            r += ', ';
+        }
+        r += ips[i];
+    }
+
+    return r;
+}
+
+function goto_server_detail(server_id) {
+    window.location = 'detail.html?server_id=' + server_id;
+}
