de7133b8a4ec4bec955624dcb1034f52e65f43a3
microcai <microcaicai@gmail.com>
2015-05-15 22:42:49 +0800

    use new domain

--- js/voeapi.js
+++ js/voeapi.js
@@ -4,7 +4,7 @@
 var api_path_base = 'api/';
 
 if (window.location.hostname == "demo.bd2.tv")
-    api_path_base = '///beansoft163com.ddns.net:8840/';
+    api_path_base = '///master.demo.bd2.tv:8840/';
 
 $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
     options.crossDomain ={
